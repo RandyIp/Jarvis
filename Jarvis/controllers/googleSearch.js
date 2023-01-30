0e7a76e937767341754c07c00c82b7252fb42eda
RandyIp <randyip9@mail.com>
2023-01-29 17:19:19 -0800

    Updating

--- Jarvis/controllers/googleSearch.js
+++ Jarvis/controllers/googleSearch.js
@@ -1,6 +1,6 @@
 const SerpApi = require('google-search-results-nodejs')
 const search = new SerpApi.GoogleSearch(process.env.TOKEN)
-
+//more tests
 const googleSearch = (req, res) => {
   console.log(req, res)
   search.json({
