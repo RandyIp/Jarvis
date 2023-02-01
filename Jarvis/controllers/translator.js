4ec7888df499badd7d30e712857ed725074d7f9f
RandyIp <randyip9@mail.com>
2023-01-31 16:10:49 -0800

    updated

--- Jarvis/controllers/translator.js
+++ Jarvis/controllers/translator.js
@@ -1,6 +1,6 @@
 pool = require('../db/pg.js')
 
-//need to update on deployment
+//need to update on deployment a few more times
 const translator = {
   post: async (req, res) => {
     result = await pool.query(`
